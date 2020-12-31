657adf5ce7b4515f64e64a7675989a1dccd1dfa4
cjwbrandon <cjwbrandon@gmail.com>
2020-12-31 16:25:16 +0800

    [ADD] More go commands

--- s3-dev-env/go.sh
+++ s3-dev-env/go.sh
@@ -7,3 +7,20 @@ go env
 # Commands
 go help
 go help [topic]
+
+# Format code
+go fmt
+go fmt ./... # Formatting entire repo
+
+# Run file
+go run {file_path}.go
+
+# Builds file, reports errors, puts executable into current folder
+go build
+
+# Compiles program (build it), names the executable the folder name holding the code, puts in workspace/bin
+go install {file_path}.go
+go install *.go
+
+# Get repo
+go get -d {url}/{user}/{repo_name}
