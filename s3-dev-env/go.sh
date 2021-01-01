6edb701c00a9c9159230807f722a1152beb567ba
cjwbrandon <cjwbrandon@gmail.com>
2021-01-01 11:42:34 +0800

    [ADD] Go Modules

--- s3-dev-env/go.sh
+++ s3-dev-env/go.sh
@@ -24,3 +24,25 @@ go install *.go
 
 # Get repo
 go get -d {url}/{user}/{repo_name}
+
+# Go Modules
+# Create a new module
+go mod init {namespace} # e.g. go mod init {domain}/{username}/{repo}
+# Creates go.mod & go.sum
+# go.mod stores the namespace
+# go.sum ensures future downloads of these modules retrieve the same bits as the first download
+
+# package-building
+go build
+go test
+
+# Print current module's dependencies
+go list -m all
+go list -m -version {module} # list available module versions
+
+# Change required version of a dependency
+go get {module}
+go get {module}@{version}
+
+# Remove unused dependencies
+go mod tidy
