dddfc3f1c6c0c2ce7c928c8f9b73e2e941dec392
cjwbrandon <cjwbrandon@gmail.com>
2021-01-02 17:42:06 +0800

    [ADD][s8][v64] switch

---
+++ s8-control-flow/64-switch.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	switch {
+	case false:
+		fmt.Println("This should not print")
+	case (2 == 4):
+		fmt.Println("This should not print as well")
+	case (3 == 3):
+		fmt.Println("This should print")
+		fallthrough
+	case (4 == 4):
+		fmt.Println("This only prints if you specify fallthrough")
+	default:
+		fmt.Println("this is default")
+	}
+
+	x := 42
+	switch x {
+	case 41:
+		fmt.Println("x is 41")
+	case 42:
+		fmt.Println("x is 42")
+		fallthrough // to default
+	default:
+		fmt.Println("This is default")
+	}
+
+	y := "abc"
+	switch y {
+	// Multiple cases with same block
+	case "a", "ab", "abc":
+		fmt.Println("hahahha")
+	default:
+		fmt.Println("hehehe")
+	}
+}
